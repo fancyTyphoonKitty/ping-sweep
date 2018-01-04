585cf535f1de58a06a653290ff4b616f86b595a5
Beau Poehls <poehlsbc12@gmail.com>
2018-01-04 07:22:16 -0600

    added dns option (perform DNS reverse lookup) and count option (Number of ping sweep interations)

--- ping-sweep.py
+++ ping-sweep.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import socket
+import time
 import subprocess
 import sys
 import argparse
@@ -20,12 +21,42 @@ def get_ips(filename):
             hostIps.append(hostIp)
             hostDescriptions.append(hostDescription)
     return hostIps, hostDescriptions
-# This command-line parsing code is provided.
-# Make a list of command line arguments, omitting the [0] element
-# which is the script itself.
+
+def pingSweep(count,filename,dns):
+    logging.info(count)
+    while count > 0:
+        try:
+            hostsFile = os.path.abspath(filename)
+            hostList, descriptionList = get_ips(hostsFile)
+        except AttributeError:
+            sys.exit(status=0)
+        '''
+        Loop through hosts array, ping each host, and return result
+        '''
+        for x in range(len(hostList)):
+            result = subprocess.Popen(
+                ['ping', '-c', '1', "-n", "-W", "2", hostList[x]],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
+            if result == 0:
+                logging.info('{} - {} is reachable'.format(hostList[x],descriptionList[x]))
+                if dns:
+                    try:
+                        hostname = socket.gethostbyaddr(hostList[x])[0]
+                        logging.info('hostname = {}'.format(hostname))
+                    except socket.herror:
+                        pass
+            else:
+                logging.critical('---> {} - {} is unreachable'.format(hostList[x],descriptionList[x]))
+                if dns:
+                    try:
+                        hostname = socket.gethostbyaddr(hostList[x])[0]
+                        logging.info('hostname = {}'.format(hostname))
+                    except socket.herror:
+                        pass
+        count -= 1
+    time.sleep(2)
 
 def main():
-
     # Set options for passing arguments to the script
     parser = argparse.ArgumentParser()
 
@@ -35,35 +66,15 @@ def main():
                     help="a string for the input file (e.g. - hosts.csv or hosts.txt")
         # - option for specifying the input filename is required
     parser.add_argument('-d', '--dns', action='store_true')
+    parser.add_argument('-c', '--count', required=False,
+                        type=int,
+                        nargs='?',
+                        default=1,
+                        const=1,
+                        help="enter number of times you'd like to perform ping sweep")
     args = parser.parse_args()
-    try:
-        hostsFile = os.path.abspath(args.filename)
-        hostList, descriptionList = get_ips(hostsFile)
-    except AttributeError:
-        sys.exit(status=0)
-    '''
-    Loop through hosts array, ping each host, and return result
-    '''
-    for x in range(len(hostList)):
-        result = subprocess.Popen(
-            ['ping', '-c', '1', "-n", "-W", "2", hostList[x]],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
-        if result == 0:
-            logging.info('{} - {} is reachable'.format(hostList[x],descriptionList[x]))
-            if args.dns:
-                try:
-                    hostname = socket.gethostbyaddr(hostList[x])[0]
-                    logging.info('hostname = {}'.format(hostname))
-                except socket.herror:
-                    pass
-        else:
-            logging.critical('---> {} - {} is unreachable'.format(hostList[x],descriptionList[x]))
-            if args.dns:
-                try:
-                    hostname = socket.gethostbyaddr(hostList[x])[0]
-                    logging.info('hostname = {}'.format(hostname))
-                except socket.herror:
-                    pass
+    pingSweep(args.count,args.filename,args.dns)
+    
 
 if __name__ == '__main__':
     main()
